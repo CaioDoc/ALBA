e9d1017b1a128aef2fbc71d650c6e54f1e05b197
CaioDoc <caio.augusto.doc@gmail.com>
2026-06-30 20:29:40 -0300

    Fix

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/ALBA", // Lembre-se de deixar o nome exato do seu repositório
-  
-  // O images fica aqui na raiz, fora de qualquer "experimental"
+  basePath: "/ALBA",
   images: {
     unoptimized: true,
   },
