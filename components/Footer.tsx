4a5d194835be3043ef6e3258a579c528cb0642bf
CaioDoc <caio.augusto.doc@gmail.com>
2026-07-14 15:32:54 -0300

    Fix - Telefone

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -56,15 +56,6 @@ export const Footer = () => {
               </svg>
               <a href="mailto:info@ayurvedica.org" className="cursor-pointer hover:text-white transition-colors">info@ayurvedica.org</a>
             </li>
-            <li className="flex items-start gap-3">
-              <svg className="w-5 h-5 text-emerald-600 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-              </svg>
-              <div className="flex flex-col gap-1">
-                <a href="tel:+351919075904" className="cursor-pointer hover:text-white transition-colors">(+351) 919 075 904</a>
-                <a href="tel:+351212467740" className="cursor-pointer hover:text-white transition-colors">(+351) 212 467 740</a>
-              </div>
-            </li>
           </ul>
         </div>
 
